958d6e5eecf8971e705c8f5ae11e17aae656a9e8
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-04-18 01:19:07 +0900

    add vuetify

---
+++ vue-form/src/components/AppNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app-bar
+      app
+      color="green"
+      dark
+    >
+      <div class="d-flex align-center">
+          농구일기
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-btn>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
